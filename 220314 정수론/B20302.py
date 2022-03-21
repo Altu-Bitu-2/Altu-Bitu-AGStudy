0b95db7e757520d904d784acee043224a07c0027
peach1510 <2m9s3b1@ewhain.net>
2022-03-21 19:02:57 +0900

    220314 정수론
    [3월 14일] 정수론 - Updated

--- 220314 정수론/B20302.py
+++ 220314 정수론/B20302.py
@@ -11,12 +11,13 @@ cal = [0] * 100001
 def prime(n):
     prime_number[1].append(1)
     for i in range(2, n+1):
+        # 소수 여부 체크
         if prime_check[i]:
             j = 2
             prime_number[i].append(i)
-            # i랑 j를 곱한 수를 전부 False로 (소수가 아니므로)
             while i * j <= n:
                 prime_check[i*j] = False
+                # i의 배수가 되는 수를 찾아 각 소수의 리스트를 더함
                 prime_number[i*j] = prime_number[i] + prime_number[j]
                 j += 1
 
@@ -24,7 +25,9 @@ def prime(n):
 # 입력된 최대 수만큼 에라토스테네스의 체로 소수 여부 배열에 저장
 prime(100000)
 
+
 def factorization(nb, op):
+    # 곱일 경우 해당 수의 소수를 +, 아니라면 -.
     if op == '*':
         for r in prime_number[nb]:
             cal[r] += 1
@@ -44,6 +47,7 @@ else:
 
     check = 0
 
+    # 음수가 하나라도 있으면 유리수
     for p in cal:
         if p < 0:
             check += 1

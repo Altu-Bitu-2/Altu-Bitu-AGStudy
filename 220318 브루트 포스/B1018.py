0e11b5ce3f90510b226daf2454e5a03b86e5f9e0
peach1510 <2m9s3b1@ewhain.net>
2022-03-26 14:16:59 +0900

    220318 브루트포스
    [3월 18일] 브루트포스 - Updated

--- 220318 브루트 포스/B1018.py
+++ 220318 브루트 포스/B1018.py
@@ -14,15 +14,8 @@ def white_chess(x, y):
         for j in range(y, y+8):
             if chess[i][j] != w[i-x][j-y]:
                 count += 1
-    return count
-
-
-def black_chess(x, y):
-    count = 0
-    for i in range(x, x+8):
-        for j in range(y, y+8):
-            if chess[i][j] != w[7-i+x][7-j+y]:
-                count += 1
+    if count > 32:
+        return 64 - count
     return count
 
 
@@ -31,10 +24,6 @@ answer = int(1e9)
 
 for i in range(n-7):
     for j in range(m-7):
-        # i와 j가 시작점, 흰색으로 시작했을 때와 검은색으로 시작했을 때 바꾸어야 할 영역의 계산해 최솟값
-        value = min(white_chess(i, j), black_chess(i, j))
-
-        if value < answer:
-            answer = value
+        answer = min(answer, white_chess(i, j))
 
 print(answer)
